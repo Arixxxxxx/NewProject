8705f4faf7d59995c8662142c1b42427cfc0c5a7
SanE <humi2001@nate.com>
2024-03-19 20:37:08 +0900

    Player Animation 적용

--- Assets/ActionManager.cs
+++ Assets/ActionManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ActionManager : MonoBehaviour
 {
     public static ActionManager inst;
-    // Start is called before the first frame update
+    bool actionStart;
     void Start()
     {
         if (inst == null)
@@ -18,7 +18,7 @@ public class ActionManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
