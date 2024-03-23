6d6624a5c16b50e3ad564a91e08961740be0ae73
ant9529 <qkrruagml@gmail.com>
2024-03-23 18:56:32 +0900

    골드 빅인트로 계산수식 구현 및 텍스트로 출력 구현

--- Assets/CODE/Manager/UIStatus.cs
+++ Assets/CODE/Manager/UIStatus.cs
@@ -5,22 +5,25 @@ using UnityEngine.UIElements;
 using TMPro;
 using System.Numerics;
 using System;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UIStatus : MonoBehaviour
 {
     [SerializeField] int Number;
     [SerializeField] int Lv;
-    [SerializeField] float baseCost;//초기 비용
-    [SerializeField] float nextCost;//다음레벨 비용
+    int LvCur = 1;
+    int itemCur = 1;
+    [SerializeField] BigInteger baseCost;//초기 비용
+    [SerializeField] BigInteger nextCost;//다음레벨 비용
     [SerializeField] float growthRate;//성장률
-    [SerializeField] float initialProd;//초기 생산량
+    [SerializeField] BigInteger initialProd;//초기 생산량
     [SerializeField] float powNum;//단계별 지수
-    [SerializeField] float totalProd;//총 생산량
+    [SerializeField] BigInteger totalProd;//총 생산량
     [SerializeField] TextMeshProUGUI priceText;
     [SerializeField] TextMeshProUGUI upGoldText;
     [SerializeField] TextMeshProUGUI LvText;
     [SerializeField] TextMeshProUGUI totalGoldText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +33,7 @@ public class UIStatus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void initValue()
@@ -38,24 +41,30 @@ public class UIStatus : MonoBehaviour
         powNum = 0;
         for (int iNum = 0; iNum <= Number; iNum++)// 단계별 지수 설정
         {
-            powNum +=  0.5f * iNum;
+            powNum += 0.3f * iNum;
         }
-        initialProd = 1.67f * Mathf.Pow(10, powNum);
-        baseCost = initialProd * 2.56f;
-        nextCost = baseCost * Mathf.Pow(growthRate, Lv);
+        int intpowNum = (int)Mathf.Floor(powNum);
+        float pracpowNum = powNum - intpowNum;
+        BigInteger temp = BigInteger.Pow(10, intpowNum);
+        float temp2 = Mathf.Pow(10, pracpowNum);
+        BigInteger resultPowNum = BigInteger.Multiply(temp, (BigInteger)temp2);
+        initialProd = BigInteger.Multiply((BigInteger)1.67f, resultPowNum);
+
+        baseCost = BigInteger.Multiply(initialProd, (BigInteger)2.56f);
+        nextCost = BigInteger.Multiply(baseCost, BigInteger.Pow((BigInteger)growthRate, Lv));
         totalProd = initialProd * Lv;
         setText();
     }
 
     private void setText()
     {
-        priceText.text = nextCost.ToString();
-        upGoldText.text = $"{initialProd * (Lv + 1) - initialProd * (Lv + 1)}";
-        LvText.text = Lv.ToString();
-        totalGoldText.text = $"GPS : {totalProd}";
+        priceText.text = CalCulator.inst.StringFourDigitChanger(nextCost.ToString());
+        upGoldText.text = CalCulator.inst.StringFourDigitChanger($"{initialProd * (Lv + 1) - initialProd * (Lv)}");
+        LvText.text = CalCulator.inst.StringFourDigitChanger(Lv.ToString());
+        totalGoldText.text ="Gps : " + CalCulator.inst.StringFourDigitChanger($"{totalProd}");
     }
 
-    private string calculatePow(float value, int pow)
+    private BigInteger calculatePow(float value, int pow)// biginteger로 float 제곱 계산기
     {
         string strValue = value.ToString();
         int count = strValue.Length;
@@ -63,7 +72,7 @@ public class UIStatus : MonoBehaviour
         char point = '.';
         for (int iNum = 0; iNum < count; iNum++)
         {
-            if (string.Equals(strValue[iNum],  point))
+            if (Equals(strValue[iNum], point))
             {
                 pointNum = iNum;
                 break;
@@ -71,27 +80,59 @@ public class UIStatus : MonoBehaviour
         }
         if (pointNum != 0)
         {
-            int countValue = strValue.Length - pointNum - 1;
-            BigInteger b = (BigInteger)(value * Mathf.Pow(10, countValue));
-            BigInteger A = BigInteger.Pow(b, pow);
-            string result = A.ToString();
-            int index = result.Length - countValue;
-            result = result.Insert(index, point.ToString());
+            int pracCount = strValue.Length - pointNum - 1;
+            float powpracCount = Mathf.Pow(10, pracCount);
+            BigInteger intvalue = (BigInteger)(value * powpracCount);
+            BigInteger powResult = BigInteger.Pow(intvalue, pow);
+            BigInteger Result = BigInteger.Divide(powResult, BigInteger.Pow((BigInteger)powpracCount, pow));
+            return Result;
+        }
+        else
+        {
+            return BigInteger.Parse(strValue);
+        }
+    }
+
+    private BigInteger multiplyBigInteger(BigInteger Ivalue, float fvalue)
+    {
+        string strValue = fvalue.ToString();
+        int count = strValue.Length;
+        int pointNum = 0;
+        char point = '.';
+        for (int iNum = 0; iNum < count; iNum++)
+        {
+            if (Equals(strValue[iNum], point))
+            {
+                pointNum = iNum;
+                break;
+            }
+        }
+        if (pointNum != 0)
+        {
+            int pracCount = strValue.Length - pointNum - 1;
+            float powpracCount = Mathf.Pow(10, pracCount);
+            BigInteger intvalue = (BigInteger)(fvalue * powpracCount);
+            BigInteger result = BigInteger.Multiply(Ivalue, intvalue);
+            result = BigInteger.Divide(result, (BigInteger)powpracCount);
             return result;
         }
         else
         {
-            return strValue;
+            BigInteger result = BigInteger.Multiply(Ivalue, (BigInteger)fvalue);
+            return result;
         }
     }
 
     public void ClickBuy()
     {
         Lv++;
-        totalProd = initialProd * Lv;
-        nextCost = baseCost * Mathf.Pow(growthRate, Lv);
-        calculatePow(growthRate, Lv);
-        Debug.Log(calculatePow(growthRate, Lv));
+        if (Lv >= 25 * LvCur)
+        {
+            LvCur *= 2;
+        }
+        totalProd = multiplyBigInteger(initialProd, Lv * LvCur * itemCur);
+        Debug.Log(totalProd);
+        nextCost = baseCost * calculatePow(growthRate, Lv);
         setText();
     }
 }
