d2089df2d7151d732b1b7940db5768afa1283479
ant9529 <qkrruagml@gmail.com>
2024-03-27 05:36:10 +0900

    골드 줄어드는 함수 제작중

--- Assets/CODE/Manager/GameStatus.cs
+++ Assets/CODE/Manager/GameStatus.cs
@@ -264,12 +264,4 @@ public class GameStatus : MonoBehaviour
         WorldUI_Manager.inst.Get_Increase_GetGoldAndStar_Font(0, CalCulator.inst.StringFourDigitChanger(getValue));
         Gold = result;
     }
-
-    public void MinusGold(string getValue)
-    {
-        string result = CalCulator.inst.DigidMinus(gold, getValue);
-        Gold = result;
-    }
-
-
 }
