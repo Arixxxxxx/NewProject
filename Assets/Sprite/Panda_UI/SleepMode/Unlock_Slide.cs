7250b514fca5cf540a93d2c5a72d74f5bf31fc54
DESKTOP-UILCHM4\humi2 <humi2001@nate.com>
2024-06-07 23:47:16 +0900

    1. 슬립모드 사운드 변경 2. 파티클 메테리얼 수정

--- Assets/Sprite/Panda_UI/SleepMode/Unlock_Slide.cs
+++ Assets/Sprite/Panda_UI/SleepMode/Unlock_Slide.cs
@@ -83,7 +83,7 @@ public class Unlock_Slide : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
         if (dragStart == false)
         {
             dragStart = true;
-            AudioManager.inst.SleepMode_SFX(2, 0.6f);
+            AudioManager.inst.SleepMode_SFX(11, 1f);
             SleepMode.inst.PandaIMGChanger(1);
         }
 
